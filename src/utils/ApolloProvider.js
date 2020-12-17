88cc99b6d89c5d0487f55af2fa8e3027840999cc
JC <james.calingo@gmail.com>
2020-12-17 15:49:04 -0500

    updating proxy

--- src/utils/ApolloProvider.js
+++ src/utils/ApolloProvider.js
@@ -3,7 +3,7 @@ import App from "../App"
 import {ApolloClient, createHttpLink, InMemoryCache, ApolloProvider} from "@apollo/client"
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001'
+  uri: 'http://localhost:3001' || 'https://theencouragementnetwork-server.herokuapp.com/'
 })
 
 const client = new ApolloClient({
