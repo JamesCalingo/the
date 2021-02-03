48904857cd03008aa8439d8c5dadb1ac65dd7e61
JC <james.calingo@gmail.com>
2021-02-03 12:09:25 -0500

    working on register and login components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
-import Story from './pages/Story'
-import MessageList from './pages/MessageList'
-import CreateMsg from './pages/CreateMsg'
-import UpdateMessage from './pages/UpdateMessage'
-import Nav from './components/Nav'
-import Welcome from './pages/Welcome'
-import Admin from './pages/Admin'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Story from "./pages/Story";
+import MessageList from "./pages/MessageList";
+import CreateMsg from "./pages/CreateMsg";
+import UpdateMessage from "./pages/UpdateMessage";
+import Nav from "./components/Nav";
+import Welcome from "./pages/Welcome";
+import Admin from "./pages/Admin";
 
-import './App.css'
+import "./App.css";
 
-function App () {
+function App() {
   return (
     <Router>
       <Nav />
 
-   
-      <Route exact path='/' component={Welcome} />
+      <Route exact path="/" component={Welcome} />
       <Route exact path="/story" component={Story} />
-      <Route exact path='/messages' component={MessageList} />
-      <Route exact path='/create' component={CreateMsg} />
-      <Route exact path='/update/:id' component={UpdateMessage} />
-      <Route exact path='/admin' component={Admin} />
-      
-   
+      <Route exact path="/messages" component={MessageList} />
+      <Route exact path="/create" component={CreateMsg} />
+      <Route exact path="/update/:id" component={UpdateMessage} />
+      <Route exact path="/admin" component={Admin} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,19 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
-function Register() {
+export default function Login() {
   return(
     <div>
-      <h1>I'm so glad you want to send out messages!</h1>
-      <h2>However, I'm going to need you to create an account in order to do so.</h2>
+       <h1>I'm so glad you want to send out messages to others!</h1>
+      <h2>However, I'm going to need you to log in in order to do so.</h2>
 
     <p>The internet, inasmuch as it's full of wonderful people like you who want to see others succeed, is also full of nasty trolls who will try to be mean or funny (or both) in order to generate negative reactions - the opposite of what I want to have happen with this site.</p>
 
     <p>This step helps us catch those trolls by tracking if people are consistently posting bad messages. I know that sounds like a bit of an invasion of privacy, but I promise I won't send you random emails or give your email to anyone.</p>
 
-   <form>
+    <p>If you need to create an account, <Link to="/register">you can register here</Link>.</p>
+
+      <form>
   <div className="mb-3">
     <label htmlFor="InputEmail" className="form-label">Email address</label>
     <input type="email" className="form-control" id="InputEmail" />
@@ -19,14 +22,9 @@ function Register() {
     <label htmlFor="inputPassword" className="form-label">Password</label>
     <input type="password" className="form-control" id="inputPassword" />
   </div>
-  <div className="mb-3">
-    <label htmlFor="confirmPW" className="form-label">Password</label>
-    <input type="password" className="form-control" id="confirmPW" />
-  </div>
+ 
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
-
-
     </div>
   )
 }
