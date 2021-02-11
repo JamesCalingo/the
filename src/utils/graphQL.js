99ea658c48ca5f36234f6b5c9a368c90be07cc80
JC <james.calingo@gmail.com>
2021-02-10 19:04:31 -0500

    adding login mutation

--- src/utils/graphQL.js
+++ src/utils/graphQL.js
@@ -1,16 +1,5 @@
 import gql from "graphql-tag"
 
-export const FETCH_MESSAGES = gql`
-  {
-    getMessages {
-      id
-      body
-      createdAt
-      likes
-      flagged
-    }
-  }
-`;
 
 export const REGISTER_USER = gql`
 mutation register(
@@ -25,3 +14,37 @@ mutation register(
     confirmPW: $confirmPW
   }){id email token}
 }`
+
+export const LOGIN = gql`
+mutation login(
+  $email: String!
+  $password: String!
+)
+{login(email: $email, password: $password){
+  token
+}}
+`
+export const FETCH_MESSAGES = gql`
+  {
+    getMessages {
+      id
+      body
+      createdAt
+      likes
+      flagged
+    }
+  }
+`;
+
+
+const POST_MESSAGE = gql`
+  mutation postMessage($body: String!) {
+    postMessage(body: $body) {
+      id
+      body
+      createdAt
+      likes
+      flagged
+    }
+  }
+`;
